b1673f9deb6ce5a1b6f3824e3ac395822711c396
yangguangzb <2570886020@qq.com>
2018-05-03 20:57:03 +0800

    redis客户端连接

--- hadoop/src/main/java/com/san/hdfs/HdfsClientDemo.java
+++ hadoop/src/main/java/com/san/hdfs/HdfsClientDemo.java
@@ -1,5 +1,27 @@
 package com.san.hdfs;
-
+import java.net.URI;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.junit.Before;
+import org.junit.Test;
 public class HdfsClientDemo {
+	FileSystem fs=null;
+	Configuration conf=null;
 	
+	@Before
+	public void init() throws Exception{
+		conf=new Configuration();
+		fs=FileSystem.get(new URI("http://mini01:9000"), conf,"hadoop");
+	}
+	
+	/**
+	 * 创建目录
+	 */
+	@Test
+	public void mkdirTest() throws Exception{
+		boolean mkdirs = fs.mkdirs(new Path("/aaa"));
+		System.out.println("mkdirs="+mkdirs);
+	}
 }
+
---
+++ redis/src/main/java/com/san/jedis/JedisClient.java
@@ -0,0 +1,42 @@
+package com.san.jedis;
+
+import org.junit.Test;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+public class JedisClient {
+	
+	/**
+	 * 单个版
+	 */
+	@Test
+	public void jedisClient(){
+		Jedis jedis = new Jedis("192.168.253.21",6379);
+		//通过set赋值
+		jedis.set("s3","3333");
+		//jedis.select("");选择数据库
+		//通过get取值
+		String result = jedis.get("s3");
+		System.out.println(result);
+		//关闭jedis
+		jedis.close();
+	}
+	
+	/**
+	 * 连接池
+	 */
+	@Test
+	public void jedisPool(){
+		JedisPool pool = new JedisPool("192.168.253.21",6379);
+		//通过连接池获取jedis对象
+		Jedis jedis = pool.getResource();
+		jedis.set("s4", "444");
+		String result = jedis.get("s4");
+		System.out.println(result);
+		//关闭
+		jedis.close();
+		pool.close();
+	}
+	
+}
